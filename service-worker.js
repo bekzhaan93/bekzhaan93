93a9e50c672a3065032253c5580d6ebabdc24e5e
bekzhaan93 <105780906+bekzhaan93@users.noreply.github.com>
2025-08-18 10:34:19 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,6 @@
 
-const CACHE_NAME = 'bekcalc-cache-v2';
-const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME ? caches.delete(k) : null)))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match('./index.html')))
-  );
-});
+const CACHE_NAME='bekcalc-unlimited-full-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const a=r.clone();caches.open(CACHE_NAME).then(cc=>cc.put(e.request,a));return r}).catch(()=>caches.match('./index.html'))))});
